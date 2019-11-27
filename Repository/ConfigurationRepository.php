b862dfcfdad7f38b031533f28255d2e391e95758
Laurent Minguet <laurent@spareka.com>
2019-11-27 10:59:09 +0100

    update symfony to 4.4 fix unit tests for symfony mailer fix issue on some repositories fix issue on deprecated doctrine bundle usage

--- Repository/ConfigurationRepository.php
+++ Repository/ConfigurationRepository.php
@@ -5,7 +5,7 @@ namespace Spipu\ConfigurationBundle\Repository;
 
 use Spipu\ConfigurationBundle\Entity\Configuration;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Configuration|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,9 +17,9 @@ class ConfigurationRepository extends ServiceEntityRepository
 {
     /**
      * ConfigurationRepository constructor.
-     * @param RegistryInterface $registry
+     * @param ManagerRegistry $registry
      */
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Configuration::class);
     }
--- Tests/Unit/Repository/ConfigurationRepositoryTest.php
+++ Tests/Unit/Repository/ConfigurationRepositoryTest.php
@@ -5,13 +5,11 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Spipu\ConfigurationBundle\Repository\ConfigurationRepository;
 use PHPUnit\Framework\TestCase;
 use Spipu\CoreBundle\Tests\SymfonyMock;
-use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class ConfigurationRepositoryTest extends TestCase
 {
     public function testRepository()
     {
-        /** @var RegistryInterface $registry */
         $repository = new ConfigurationRepository(SymfonyMock::getEntityRegistry($this));
 
         $this->assertInstanceOf(ServiceEntityRepository::class, $repository);
