61e6495d4d1a01ecbe2e28637e9f0dc8478ab5c8
Laurent Minguet <laurent@spareka.com>
2021-07-22 10:42:46 +0200

    better PSR12

--- Tests/SpipuConfigurationMock.php
+++ Tests/SpipuConfigurationMock.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * This file is part of a Spipu Bundle
+ *
+ * (c) Laurent Minguet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Spipu\ConfigurationBundle\Tests;
 
 use PHPUnit\Framework\MockObject\MockObject;
