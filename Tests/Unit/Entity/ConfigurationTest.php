712c38c1196431990c439fccb85582b1e0ea2339
spipu <spipu45@gmail.com>
2023-04-01 22:13:09 +0200

    add some unit tests

--- Tests/Unit/Entity/ConfigurationTest.php
+++ Tests/Unit/Entity/ConfigurationTest.php
@@ -16,6 +16,14 @@ class ConfigurationTest extends TestCase
         $entity->setValue('value');
         $this->assertSame('value', $entity->getValue());
 
+        $this->assertSame(null, $entity->getScope());
+        $entity->setScope('');
+        $this->assertSame(null, $entity->getScope());
+        $entity->setScope('foo');
+        $this->assertSame('foo', $entity->getScope());
+        $entity->setScope(null);
+        $this->assertSame(null, $entity->getScope());
+
         $setId = \Closure::bind(
             function ($value) {
                 $this->id = $value;
